8b1d485aa9d9733a06c99d32dabf951b86bfd78b
Mario <mamerida2013@gmail.com>
2021-12-27 18:24:39 -0300

    conexion con la api de unsplash obtencion de las imagenes con useState y verificacion de persistencia de las fotos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import {Formik , Form, Field} from 'formik';
+import { useState } from 'react';
 import './header.css'
 
 const  App = () => {
+  //para poder guardar las ftos traidas por la API uso useState para poder persistir las imagenes 
+  const [photos,setPhotos] = useState([])
+  console.log({photos})
   return (
     <div>
       <header>
@@ -10,7 +14,15 @@ const  App = () => {
           initialValues={{search:""}}
           onSubmit={ async values =>{
               //llamar API de unsplash
-              console.log(values)
+              //esto lo hago mediante await y fecth con la url de la pagina web haciendo el llamdo a la api y el objeto de configuracion
+              const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+               { headers:{
+                    'Authorization':'Client-ID IqmgW4YAw7jTfvLaH3azt3uuEpo3W5XBkZPdFZcFn7Q'
+                    }
+              
+                })
+                const data = await response.json()
+              setPhotos(data.results)
           }}
         >
           <Form>
